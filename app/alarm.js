7d747a51988498fc8a5e8893c74d12bdd91e51b0
etfeire <etfeire@gmail.com>
2019-03-26 20:12:30 +0000

    first commit

--- app/alarm.js
+++ app/alarm.js
@@ -11,22 +11,42 @@ function wakeup(currentTime) {
   // https://www.w3schools.com/jsref/jsref_obj_date.asp
   const currentHour = currentTime.getHours();
 
-  if (currentHour > 12) {
-    return "Ugggggh its too early - I'm not waking up yet";
-  } else {
-    return "Get up!! You're wasting the day";
+  let text = "0"
+  switch (currentHour) {
+    case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11:
+      text = "Ugggggh its too early - I'm not waking up yet";
+      return text;
+      break;
+    case 12:
+    case 13:
+    case 14:
+      text = "Yessssss its lunch time!!!";
+      return text;
+      break;
+    default:
+      text = "Get up!! You're wasting the day";
+      return text;
+      break;
   }
 }
 
 function snooze() {
   let snoozeAttempts = 0;
 
-  for (let i = 0; i < 10; i++) {
+  let i = 0;
+  while (i < 20) {
+    i++;
     snoozeAttempts++;
   }
 
   return snoozeAttempts;
 }
+
+
+peopleThatHaveUsedAlarm.forEach(function(obj) {
+    console.log(obj["name"]);
+  }
+});
 
 module.exports = {
   wakeup,
--- test/alarm.test.js
+++ test/alarm.test.js
@@ -6,6 +6,22 @@ test('Moans if too early', () => {
   expect(alarm.wakeup(timeNow)).toBe("Ugggggh its too early - I'm not waking up yet");
 });
 
+test('Wasting the day', () => {
+  let timeNow = new Date();
+  timeNow.setHours(15);
+  expect(alarm.wakeup(timeNow)).toBe("Get up!! You're wasting the day");
+});
+
+test('Lunchtime', () => {
+  let timeNow = new Date();
+  timeNow.setHours(12) || timeNow.setHours(13) || timeNow.setHours(14);
+  expect(alarm.wakeup(timeNow)).toBe("Yessssss its lunch time!!!");
+});
+
 test('Snoozing for a set amount', () => {
-  expect(alarm.snooze()).toBe(10);
+  expect(alarm.snooze()).toBe(20);
+});
+
+test('Call names', () => {
+  expect(people("name")).toBe("James");
 });
